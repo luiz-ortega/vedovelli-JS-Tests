e39711cf1b55995925e128890f2a6803ab56d46a
luiz-ortega <luizngust@hotmail.com>
2020-10-12 16:37:38 -0300

    test: params types as array and object.

--- module1/project1/src/lib/queryString.spec.js
+++ module1/project1/src/lib/queryString.spec.js
@@ -1,12 +1,35 @@
 const { queryString } = require('./queryString');
 
 describe('Objet to query string', () => {
-  it('should create a valida query string when an object is provided', () => {
+  it('should create a valid query string when an object is provided', () => {
     const obj = {
       name: 'Luiz',
       profession: 'Developer',
     };
 
     expect(queryString(obj)).toBe('name=Luiz&profession=Developer');
+  });
+
+  it('should create a valid query string event when an array value is passed', () => {
+    const obj = {
+      name: 'Luiz',
+      abilities: ['JS', 'TDD'],
+    };
+
+    expect(queryString(obj)).toBe('name=Luiz&abilities=JS,TDD');
+  });
+
+  it('should throw an error when an object is passsed as value', () => {
+    const obj = {
+      name: 'Luiz',
+      abilities: {
+        first: 'JS',
+        second: 'TDD',
+      },
+    };
+
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
   });
 });
