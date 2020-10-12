338d6413cf936017d2555b0046e2e2718877d595
luiz-ortega <luizngust@hotmail.com>
2020-10-12 17:42:18 -0300

    test: coverage

--- module1/project1/src/lib/queryString.spec.js
+++ module1/project1/src/lib/queryString.spec.js
@@ -1,4 +1,4 @@
-const { queryString } = require('./queryString');
+const { queryString, parse } = require('./queryString');
 
 describe('Objet to query string', () => {
   it('should create a valid query string when an object is provided', () => {
@@ -31,5 +31,33 @@ describe('Objet to query string', () => {
     expect(() => {
       queryString(obj);
     }).toThrowError();
+  });
+});
+
+describe('Query string to object', () => {
+  it('should convert a query string to object', () => {
+    const qs = 'name=Luiz&profession=Developer';
+
+    expect(parse(qs)).toEqual({
+      name: 'Luiz',
+      profession: 'Developer',
+    });
+  });
+
+  it('should convert a query string of a single key-value pair', () => {
+    const qs = 'name=Luiz';
+
+    expect(parse(qs)).toEqual({
+      name: 'Luiz',
+    });
+  });
+
+  it('should convert a query string to an object taking care of coma separating values', () => {
+    const qs = 'name=Luiz&abilities=JS,TDD';
+
+    expect(parse(qs)).toEqual({
+      name: 'Luiz',
+      abilities: ['JS', 'TDD'],
+    });
   });
 });
