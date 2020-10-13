e3f43c4c94b591a014705127d21141b1b0a4731e
luiz-ortega <luizngust@hotmail.com>
2020-10-13 20:42:42 -0300

    test: Cart remove item.

--- module1/project1/src/lib/Cart.spec.js
+++ module1/project1/src/lib/Cart.spec.js
@@ -3,6 +3,16 @@ import Cart from './Cart';
 describe('Cart', () => {
     let cart;
 
+    let product = {
+        title: 'Adidas running shoes - men',
+        price: 35388,
+    };
+
+    let product2 = {
+        title: 'Adidas running shoes - women',
+        price: 41872,
+    };
+
     beforeEach(() => {
         cart = new Cart();
     });
@@ -13,15 +23,42 @@ describe('Cart', () => {
 
     it('should multiply quantity and price and receive the total amount', () => {
         const item = {
-            product: {
-                title: 'Adidas running shoes - men',
-                price: 35388,
-            },
+            product,
             quantity: 2,
         };
 
         cart.add(item);
 
         expect(cart.getTotal()).toEqual(70776);
+    });
+
+    it('should ensure no more than one product exists at a time', () => {
+        cart.add({
+            product,
+            quantity: 2,
+        });
+
+        cart.add({
+            product,
+            quantity: 1,
+        });
+
+        expect(cart.getTotal()).toEqual(35388);
+    });
+
+    it('should update total when a product gets included and then removed', () => {
+        cart.add({
+            product,
+            quantity: 2,
+        });
+
+        cart.add({
+            product: product2,
+            quantity: 1,
+        });
+
+        cart.remove(product);
+
+        expect(cart.getTotal()).toEqual(41872);
     });
 });
