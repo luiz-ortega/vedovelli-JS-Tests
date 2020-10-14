0a5b8b669bab83c3bbb8a8fe7aedab15bfa57536
luiz-ortega <luizngust@hotmail.com>
2020-10-14 20:38:30 -0300

    refactor: getTotal, calculatePercentageDiscount and calculateQuantityDiscount methods.

--- module1/project1/src/lib/Cart.js
+++ module1/project1/src/lib/Cart.js
@@ -2,16 +2,16 @@ import find from 'lodash/find';
 import remove from 'lodash/remove';
 import Dinero from 'dinero.js';
 
-const calculatePercentageDiscount = (amount, item) => {
-    if (item.condition?.percentage && item.quantity > item.condition.minimun) {
-        return amount.percentage(item.condition.percentage);
+const calculatePercentageDiscount = (amount, { condition, quantity }) => {
+    if (condition?.percentage && quantity > condition.minimun) {
+        return amount.percentage(condition.percentage);
     }
     return Money({ amount: 0 });
 };
 
-const calculateQuantityDiscount = (amount, item) => {
-    const isEven = item.quantity % 2 === 0;
-    if (item.condition?.quantity && item.quantity > item.condition.quantity) {
+const calculateQuantityDiscount = (amount, { condition, quantity }) => {
+    const isEven = quantity % 2 === 0;
+    if (condition?.quantity && quantity > condition.quantity) {
         return amount.percentage(isEven ? 50 : 40);
     }
     return Money({ amount: 0 });
@@ -60,25 +60,15 @@ export default class Cart {
     }
 
     getTotal() {
-        return this.items.reduce((acc, item) => {
+        return this.items.reduce((acc, { quantity, product, condition }) => {
             const amount = Money({
-                amount: item.quantity * item.product.price,
+                amount: quantity * product.price,
             });
 
             let discount = Money({ amount: 0 });
 
-            if (item.condition) {
-                discount = calculateDiscount(
-                    amount,
-                    item.quantity,
-                    item.condition,
-                );
-            }
-
-            if (item.condition?.percentage) {
-                discount = calculatePercentageDiscount(amount, item);
-            } else if (item.condition?.quantity) {
-                discount = calculateQuantityDiscount(amount, item);
+            if (condition) {
+                discount = calculateDiscount(amount, quantity, condition);
             }
 
             return acc.add(amount).subtract(discount);
