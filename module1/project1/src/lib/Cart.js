a22ebdf8dfb94c6a831db2442acbcb8adb121145
luiz-ortega <luizngust@hotmail.com>
2020-10-14 21:01:32 -0300

    refactor: discount.utils.js

--- module1/project1/src/lib/Cart.js
+++ module1/project1/src/lib/Cart.js
@@ -2,42 +2,7 @@ import find from 'lodash/find';
 import remove from 'lodash/remove';
 import Dinero from 'dinero.js';
 
-const calculatePercentageDiscount = (amount, { condition, quantity }) => {
-    if (condition?.percentage && quantity > condition.minimun) {
-        return amount.percentage(condition.percentage);
-    }
-    return Money({ amount: 0 });
-};
-
-const calculateQuantityDiscount = (amount, { condition, quantity }) => {
-    const isEven = quantity % 2 === 0;
-    if (condition?.quantity && quantity > condition.quantity) {
-        return amount.percentage(isEven ? 50 : 40);
-    }
-    return Money({ amount: 0 });
-};
-
-const calculateDiscount = (amount, quantity, condition) => {
-    const list = Array.isArray(condition) ? condition : [condition];
-
-    const [higherDiscount, test] = list
-        .map(cond => {
-            if (cond.percentage) {
-                return calculatePercentageDiscount(amount, {
-                    condition: cond,
-                    quantity,
-                }).getAmount();
-            } else if (cond.quantity) {
-                return calculateQuantityDiscount(amount, {
-                    condition: cond,
-                    quantity,
-                }).getAmount();
-            }
-        })
-        .sort((a, b) => b - a);
-
-    return Money({ amount: higherDiscount });
-};
+import { calculateDiscount } from './discount.utils';
 
 const Money = Dinero;
 
@@ -75,7 +40,7 @@ export default class Cart {
         }, Money({ amount: 0 }));
     }
 
-    sumary() {
+    summary() {
         const total = this.getTotal();
         const formatted = total.toFormat('$0,0.00');
         const items = this.items;
@@ -88,7 +53,7 @@ export default class Cart {
     }
 
     checkout() {
-        const { total, items } = this.sumary();
+        const { total, items } = this.summary();
 
         this.items = [];
 
