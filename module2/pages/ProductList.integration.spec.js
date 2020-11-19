ee654477941f5c1a72afd237f0384ef3cbf92d4b
luiz-ortega <luizngust@hotmail.com>
2020-11-18 21:59:37 -0300

    test: display error message when api call fail

--- module2/pages/ProductList.integration.spec.js
+++ module2/pages/ProductList.integration.spec.js
@@ -53,8 +53,21 @@ describe('ProductList - integration', () => {
 
     await Vue.nextTick();
 
-    // const wrapper = mount(ProductList);
     const cards = wrapper.findAllComponents(ProductCard);
     expect(cards).toHaveLength(10);
+  });
+
+  it('should display the error message when Promise rejects', async () => {
+    axios.get.mockReturnValue(Promise.reject(new Error('')));
+
+    const wrapper = mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    });
+
+    await Vue.nextTick();
+
+    expect(wrapper.text()).toContain('Problemas ao carregar a list');
   });
 });
