36f192f14275a48b9980fd08eea628fa4adfe89b
luiz-ortega <luizngust@hotmail.com>
2020-11-20 22:44:33 -0300

    test: add cart component and open/close functionality

---
+++ module2/components/Cart.init.spec.js
@@ -0,0 +1,20 @@
+import { mount } from '@vue/test-utils';
+import Cart from '@/components/Cart';
+
+describe('Cart', () => {
+  it('should mount the component', () => {
+    const wrapper = mount(Cart);
+
+    expect(wrapper.vm).toBeDefined();
+  });
+
+  it('should emit close event when button gets clicked', async () => {
+    const wrapper = mount(Cart);
+    const button = wrapper.find('[data-testid=close-button]');
+
+    await button.trigger('click');
+
+    expect(wrapper.emitted().close).toBeTruthy();
+    expect(wrapper.emitted().close).toHaveLength(1);
+  });
+});
