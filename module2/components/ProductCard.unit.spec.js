dd3546d4df8ec7bd36d8300366c9e1c4e695fbbb
luiz-ortega <luizngust@hotmail.com>
2020-11-22 18:24:05 -0300

    test: add product to Cart list when button is clicked

--- module2/components/ProductCard.unit.spec.js
+++ module2/components/ProductCard.unit.spec.js
@@ -1,6 +1,7 @@
 import { mount } from '@vue/test-utils';
 import ProductCard from '@/components/ProductCard';
 import { makeServer } from '@/miragejs/server';
+import { cartState } from '@/state';
 
 describe('ProductCard - unit', () => {
   const mountProductCard = () => {
@@ -44,13 +45,11 @@ describe('ProductCard - unit', () => {
     expect(wrapper.text()).toContain('$24.00');
   });
 
-  it('should emit the event addToCart with product object when button gets clicked', async () => {
-    const { wrapper, product } = mountProductCard();
+  it('should add item to cartState on button click', async () => {
+    const { wrapper } = mountProductCard();
 
     await wrapper.find('button').trigger('click');
 
-    expect(wrapper.emitted().addToCart).toBeTruthy();
-    expect(wrapper.emitted().addToCart.length).toBe(1);
-    expect(wrapper.emitted().addToCart[0]).toEqual([{ product }]);
+    expect(cartState.items).toHaveLength(1);
   });
 });
