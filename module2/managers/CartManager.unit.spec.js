da10459fd60294b3dff8583bfeb15d984c927531
luiz-ortega <luizngust@hotmail.com>
2020-11-23 22:54:29 -0300

    test: add productIsInTheCart method to CartManager

--- module2/managers/CartManager.unit.spec.js
+++ module2/managers/CartManager.unit.spec.js
@@ -40,5 +40,10 @@ describe('CartManager', () => {
 
   it.todo('should return true if cart is not empty');
 
-  it.todo('should return true if product is already in the cart');
+  it('should return true if product is already in the cart', () => {
+    const product = server.create('product');
+    manager.addProduct(product);
+
+    expect(manager.productIsInTheCart(product)).toBe(true);
+  });
 });
