8620efa81864fbe5294743d47710e0ac5d6b5411
luiz-ortega <luizngust@hotmail.com>
2020-11-29 14:45:56 -0300

    chore: add nuxt and cypress config for fetch products from miragejs while testing

--- module2/cypress/e2e/Store.spec.js
+++ module2/cypress/e2e/Store.spec.js
@@ -1,5 +1,18 @@
+import { makeServer } from '../../miragejs/server';
+
 context('Store', () => {
+  let server;
+
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' });
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
   it('should display the store', () => {
+    // server.createList('product', 10);
     cy.visit('http://localhost:3000/');
 
     cy.get('body').contains('Brand');
