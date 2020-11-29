7d66ece0387edbec1741a00aa58bcf974539749c
luiz-ortega <luizngust@hotmail.com>
2020-11-29 15:25:08 -0300

    test: add search products functionality e2e tests

--- module2/cypress/e2e/Store.spec.js
+++ module2/cypress/e2e/Store.spec.js
@@ -1,3 +1,4 @@
+/// <reference types="cypress" />;
 import { makeServer } from '../../miragejs/server';
 
 context('Store', () => {
@@ -12,10 +13,35 @@ context('Store', () => {
   });
 
   it('should display the store', () => {
-    // server.createList('product', 10);
     cy.visit('http://localhost:3000/');
 
     cy.get('body').contains('Brand');
     cy.get('body').contains('Wrist Watch');
+  });
+
+  context('Store > Search for products', () => {
+    it.only('should type in the search field', () => {
+      cy.visit('http://localhost:3000/');
+
+      cy.get('input[type="search"]')
+        .type('Some text here')
+        .should('have.value', 'Some text here');
+    });
+
+    it.only('should find product when product name is passed on search field', () => {
+      server.createList('product', 10);
+
+      server.create('product', {
+        title: 'Relógio bonito',
+      });
+
+      cy.visit('http://localhost:3000/');
+
+      cy.get('input[type="search"]').type('Relógio bonito');
+
+      cy.get('[data-testid="search-form"]').submit();
+
+      cy.get('[data-testid="product-card"]').should('have.length', 1);
+    });
   });
 });
