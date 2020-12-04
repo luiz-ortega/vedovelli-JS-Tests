03045eb3ba60b29e30a23a5bdd24ed87b0abfde0
luiz-ortega <luizngust@hotmail.com>
2020-12-03 21:15:27 -0300

    feat: add class hidden too clear cart button when cart is not empty

--- module2/components/Cart.vue
+++ module2/components/Cart.vue
@@ -7,7 +7,11 @@
   >
     <div class="flex items-center justify-between">
       <h3 class="text-2xl font-medium text-gray-700">Your cart</h3>
-      <button data-testid="clear-cart-button" @click="$cart.clearProducts()">
+      <button
+        :class="{ hidden: !hasProducts }"
+        data-testid="clear-cart-button"
+        @click="$cart.clearProducts()"
+      >
         clear cart
       </button>
       <button
