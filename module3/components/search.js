b71b61c05c470a4f0e268ad4736c7a0f20902f1a
luiz-ortega <luizngust@hotmail.com>
2020-12-06 20:09:55 -0300

    feat: alter input type to search

--- module3/components/search.js
+++ module3/components/search.js
@@ -25,7 +25,7 @@ export default function Search({ doSearch }) {
         value={term}
         onChange={(e) => setTerm(e.target.value)}
         className="w-full border rounded-md pl-10 pr-4 py-2 focus:border-blue-500 focus:outline-none focus:shadow-outline"
-        type="text"
+        type="search"
         placeholder="Search"
       />
     </form>
