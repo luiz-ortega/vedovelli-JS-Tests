14084c52c083333a6a2c24da35cf1a8f826312a3
luiz-ortega <luizngust@hotmail.com>
2020-12-06 19:37:42 -0300

    test: declare doSearch function as prop on Search component

--- module3/components/search.spec.js
+++ module3/components/search.spec.js
@@ -1,10 +1,22 @@
 import Search from "./search";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+
+const doSearch = jest.fn();
 
 describe("Search", () => {
   it("should render a form", () => {
-    render(<Search />);
+    render(<Search doSearch={doSearch} />);
 
     expect(screen.getByRole("form")).toBeInTheDocument();
+  });
+
+  it("should call props.doSearch() when form is submitted", async () => {
+    render(<Search doSearch={doSearch} />);
+
+    const form = screen.getByRole("form");
+
+    await fireEvent.submit(form);
+
+    expect(doSearch).toHaveBeenCalledTimes(1);
   });
 });
