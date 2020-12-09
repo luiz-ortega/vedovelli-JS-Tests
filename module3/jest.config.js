343d05aaf30a87a25237246400d5a403a21b7c9b
luiz-ortega <luizngust@hotmail.com>
2020-12-09 20:20:47 -0300

    chore: add test coverage configs

--- module3/jest.config.js
+++ module3/jest.config.js
@@ -4,9 +4,9 @@ module.exports = {
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
   },
-  //   collectCoverageFrom: [
-  //     "<rootDir>/components/**/*.js",
-  //     "<rootDir>/pages/**/*.js",
-  //     "<rootDir>/hooks/**/*.js",
-  //   ],
+  collectCoverageFrom: [
+    "<rootDir>/components/**/*.js",
+    "<rootDir>/pages/**/*.js",
+    // "<rootDir>/hooks/**/*.js",
+  ],
 };
