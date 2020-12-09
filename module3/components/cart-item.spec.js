974fdf5981e7598d85471394df9f80d615587554
luiz-ortega <luizngust@hotmail.com>
2020-12-09 20:46:09 -0300

    chore: remove unused mock function

--- module3/components/cart-item.spec.js
+++ module3/components/cart-item.spec.js
@@ -8,8 +8,6 @@ const product = {
     "https://images.unsplash.com/photo-1495856458515-0637185db551?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
 };
 
-const addToCart = jest.fn();
-
 const renderCartItem = () => {
   render(<CartItem product={product} addToCart={addToCart} />);
 };
