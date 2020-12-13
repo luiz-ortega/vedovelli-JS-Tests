40413a39de933f526792518d0e33d1348a3c365e
luiz-ortega <luizngust@hotmail.com>
2020-12-13 17:29:12 -0300

    feat: render error message when promise rejects

--- module3/pages/index.js
+++ module3/pages/index.js
@@ -8,13 +8,20 @@ export default function Home() {
   const { products, error } = useFetchProducts();
 
   const renderProductListOrMessage = () => {
-    if (products.length === 0) {
+    if (products.length === 0 && !error) {
       return <h4 data-testid="no-products">No Products</h4>;
     }
 
     return products.map((product) => (
       <ProductCard product={product} key={product.id} />
     ));
+  };
+
+  const renderErrorMessage = () => {
+    if (!error) {
+      return null;
+    }
+    return <h4 data-testid="server-error">Server is down</h4>;
   };
 
   return (
@@ -24,6 +31,7 @@ export default function Home() {
         <h3 className="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
         <span className="mt-3 text-sm text-gray-500">200+ Products</span>
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
+          {renderErrorMessage()}
           {renderProductListOrMessage()}
         </div>
       </div>
