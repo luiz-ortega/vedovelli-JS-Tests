1231be23978746d82b98849f41db3f22aec9b7da
luiz-ortega <luizngust@hotmail.com>
2020-12-15 21:42:35 -0300

    feat: render product quantity with products or product sufix correctly

--- module3/pages/index.js
+++ module3/pages/index.js
@@ -38,6 +38,14 @@ export default function Home() {
     return <h4 data-testid="server-error">Server is down</h4>;
   };
 
+  const renderProductQuantity = () => {
+    if (localProducts.length === 1) {
+      return `${localProducts.length} Product`;
+    } else {
+      return `${localProducts.length} Products`;
+    }
+  };
+
   return (
     <main data-testid="product-list" className="my-8">
       <Search
@@ -48,7 +56,7 @@ export default function Home() {
       <div className="container mx-auto px-6">
         <h3 className="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
         <span className="mt-3 text-sm text-gray-500">
-          {localProducts.length} Products
+          {renderProductQuantity()}
         </span>
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
           {renderErrorMessage()}
