cfaa6875cdf3698be22e69c0575a76492c7299fb
luiz-ortega <luizngust@hotmail.com>
2020-12-15 21:15:37 -0300

    feat: show product quantity dinamicaly when search is triggered

--- module3/pages/index.js
+++ module3/pages/index.js
@@ -47,7 +47,9 @@ export default function Home() {
       />
       <div className="container mx-auto px-6">
         <h3 className="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
-        <span className="mt-3 text-sm text-gray-500">200+ Products</span>
+        <span className="mt-3 text-sm text-gray-500">
+          {localProducts.length} Products
+        </span>
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
           {renderErrorMessage()}
           {renderProductListOrMessage()}
