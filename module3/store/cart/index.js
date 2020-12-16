68be0b9351497daf024227f38aa0b1217302e012
luiz-ortega <luizngust@hotmail.com>
2020-12-16 20:31:50 -0300

    chore: change editorconfig ident_size to 2

--- module3/store/cart/index.js
+++ module3/store/cart/index.js
@@ -1,10 +1,10 @@
 import create from "zustand";
 
 export const useCartStore = create((set) => ({
-    state: {
-        open: false,
-    },
-    actions: {
-        toggle: () => set((store) => ({ state: { open: !store.state.open } })),
-    },
+  state: {
+    open: false,
+  },
+  actions: {
+    toggle: () => set((store) => ({ state: { open: !store.state.open } })),
+  },
 }));
