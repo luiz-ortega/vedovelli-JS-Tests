8b91e290b81d8e59f88d1dc6ce1daae542a22a44
luiz-ortega <luizngust@hotmail.com>
2020-12-17 19:01:51 -0300

    feat: add initial products as an empty array

--- module3/store/cart/index.js
+++ module3/store/cart/index.js
@@ -3,6 +3,7 @@ import create from "zustand";
 export const useCartStore = create((set) => ({
   state: {
     open: false,
+    products: [],
   },
   actions: {
     toggle: () => set((store) => ({ state: { open: !store.state.open } })),
