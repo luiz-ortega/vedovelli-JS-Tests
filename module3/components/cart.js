229c4f9890fbb3b88015012916fee43c7cedeee9
luiz-ortega <luizngust@hotmail.com>
2020-12-20 13:36:01 -0300

    feat: show no products message when cart is empty

--- module3/components/cart.js
+++ module3/components/cart.js
@@ -5,6 +5,8 @@ export default function Cart() {
   const { open, products } = useCartStore((store) => store.state);
   const toggle = useCartStore((store) => store.actions.toggle);
 
+  const hasProducts = products.length > 0;
+
   return (
     <div
       data-testid="cart"
@@ -33,24 +35,33 @@ export default function Cart() {
         </button>
       </div>
       <hr className="my-3" />
+
+      {!hasProducts && (
+        <h3 className="text-center font-bold text-x1 text-blue-600">
+          There are no items in the cart
+        </h3>
+      )}
+
       {products.map((product) => (
         <CartItem product={product} key={product.id} />
       ))}
 
-      <a className="flex items-center justify-center mt-4 px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
-        <span>Checkout</span>
-        <svg
-          className="h-5 w-5 mx-2"
-          fill="none"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
-        </svg>
-      </a>
+      {hasProducts && (
+        <a className="flex items-center justify-center mt-4 px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+          <span>Checkout</span>
+          <svg
+            className="h-5 w-5 mx-2"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
+          </svg>
+        </a>
+      )}
     </div>
   );
 }
