cac5e8dbe9a98ae1e3357ba756d9e2f115a65a6a
luiz-ortega <luizngust@hotmail.com>
2021-03-09 22:40:11 -0300

    chore: use logger on app.js first function

--- module4/src/app.js
+++ module4/src/app.js
@@ -5,6 +5,7 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { attachRouters } from '@/routes';
+import { logger as appLogger } from './utils';
 
 const app = express();
 
@@ -23,6 +24,7 @@ attachRouters(app);
  * é um local centralizado para o gerenciamento de erros.
  */
 app.use((error, req, res, next) => {
+  appLogger.error(error.stack);
   res.status(error.status).json({ message: error.message });
 });
 
